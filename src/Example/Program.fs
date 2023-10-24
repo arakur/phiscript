e86b1beda6f63e566db18e8a84db17d695615c32
arakur <arakur65536@gmail.com>
2023-10-24 16:29:56 +0900

    Implemented transpiler.

--- src/Example/Program.fs
+++ src/Example/Program.fs
@@ -1,4 +1,9 @@
-﻿type DigitSeq = DigitSeq of digits: string
+﻿type DigitSeq =
+    | DigitSeq of digits: string
+
+    static member unwrap(DigitSeq digits) = digits
+
+    member this.Unwrap = DigitSeq.unwrap this
 
 type Numeral =
     { Integer: DigitSeq
@@ -13,7 +18,19 @@ type Numeral =
     static member mk integer decimal =
         { Integer = integer; Decimal = decimal }
 
-type StringLit = StringLit of content: string
+    static member compose(this: Numeral) =
+        match this.Decimal with
+        | None -> this.Integer.Unwrap
+        | Some decimal -> this.Integer.Unwrap + "." + decimal.Unwrap
+
+    member this.Compose = Numeral.compose this
+
+type StringLit =
+    | StringLit of content: string
+
+    static member compose(StringLit content) = content |> (sprintf "\"%s\"")
+
+    member this.Compose = StringLit.compose this
 
 type VarName =
     { Name: string }
@@ -31,7 +48,19 @@ type Var =
     | NoNamespace of VarName
     | Namespace of NamespaceVarName
 
-type UnOpName = UnOpName of string
+    static member compose(this: Var) =
+        match this with
+        | NoNamespace name -> name.Name
+        | Namespace name -> name.Path |> String.concat ":" |> (fun path -> path + ":" + name.Name)
+
+    member this.Compose = Var.compose this
+
+type UnOpName =
+    | UnOpName of string
+
+    static member unwrap(UnOpName name) = name
+
+    member this.Unwrap = UnOpName.unwrap this
 
 type BinOpName = BinOpName of string
 
@@ -40,10 +69,20 @@ type UnOp =
 
     static member mk name = { Name = name }
 
+    static member compose this = this.Name.Unwrap
+
+    member this.Compose = UnOp.compose this
+
 type BinOp =
     { Name: BinOpName }
 
     static member mk names = { Name = names }
+
+    static member compose(this: BinOp) =
+        match this.Name with
+        | BinOpName name -> name
+
+    member this.Compose = BinOp.compose this
 
 [<RequireQualifiedAccess>]
 type Pattern =
@@ -481,11 +520,125 @@ module Parser =
 
 //
 
+module Transpiler =
+    let rec private transpilePattern (pattern: Pattern) =
+        match pattern with
+        | Pattern.Numeral numeral -> numeral.Compose
+        | Pattern.StringLit stringLit -> stringLit.Compose
+        | Pattern.Variable var -> var.Compose
+        | Pattern.Array array ->
+            array
+            |> List.map transpilePattern
+            |> String.concat ", "
+            |> (fun s -> "[" + s + "]")
+        | Pattern.Tuple tuple -> failwith "Not Implemented"
+        | Pattern.Wildcard -> "*"
+
+    let rec private transpileExpr (expr: Expr) =
+        match expr with
+        | Expr.Numeral numeral -> numeral.Compose
+        | Expr.StringLit stringLit -> stringLit.Compose
+        | Expr.Variable var -> var.Compose
+        | Expr.Array array -> array |> List.map transpileExpr |> String.concat " " |> (fun s -> "[" + s + "]")
+        | Expr.Tuple tuple -> tuple |> List.map transpileExpr |> String.concat " " |> (fun s -> "[" + s + "]")
+        | Expr.UnOp(_) -> failwith "Not Implemented"
+        | Expr.UnOpApplied(op, arg) -> [ op.Compose; "(" + transpileExpr arg + ")" ] |> String.concat " "
+        | Expr.BinOp(_) -> failwith "Not Implemented"
+        | Expr.BinOpApplied(op, lhs, rhs) ->
+            [ "(" + transpileExpr lhs + ")"; op.Compose; "(" + transpileExpr rhs + ")" ]
+            |> String.concat " "
+        | Expr.Apply(fun_, args) ->
+            // TODO: 現状の実装は別の場所で定義したタプルを入力した場合に正しくならないので，型情報を使って正しくする．
+            match args with
+            | [ Expr.Tuple tuple ] ->
+                let fun_' = transpileExpr fun_
+
+                let args' =
+                    tuple
+                    |> List.map transpileExpr
+                    |> String.concat ", "
+                    |> (fun s -> "(" + s + ")")
+
+                fun_' + args'
+            | _ ->
+                let fun_' = transpileExpr fun_
+
+                let args' =
+                    args
+                    |> List.map transpileExpr
+                    |> List.map (fun s -> "(" + s + ")")
+                    |> String.concat ""
+
+                fun_' + args'
+        | Expr.Block block -> block |> transpileBlock
+        | Expr.If(cases, else_) ->
+            match cases with
+            | [] -> failwith "Unexpected: if with no cases."
+            | (ifCond, ifBlock) :: elifCondBlocks ->
+                let if' = [ "if"; ifCond |> transpileExpr; ifBlock |> transpileBlock ]
+
+                let elif' =
+                    elifCondBlocks
+                    |> List.collect (fun (cond, block) -> [ "elif"; cond |> transpileExpr; block |> transpileBlock ])
+
+                let else' =
+                    match else_ with
+                    | None -> []
+                    | Some block -> [ "else"; block |> transpileBlock ]
+
+                if' @ elif' @ else' |> String.concat " "
+        | Expr.Match(expr, cases) -> failwith "Not Implemented"
+
+    and private transpileStatement (statement: Statement) =
+        match statement with
+        | Do expr
+        | RawExpr expr -> transpileExpr expr
+        | Let(pat, expr) ->
+            match pat with
+            | Pattern.Variable var -> [ "let"; var.Compose; "="; transpileExpr expr ] |> String.concat " "
+            | _ -> failwith "Not Implemented"
+        | Var(pat, expr) ->
+            match pat with
+            | Pattern.Variable var -> [ "var"; var.Compose; "="; transpileExpr expr ] |> String.concat " "
+            | _ -> failwith "Not Implemented"
+        | Gets(pat, expr) ->
+            match pat with
+            | Pattern.Variable var -> [ var.Compose; "="; transpileExpr expr ] |> String.concat " "
+            | _ -> failwith "Not Implemented"
+        | For(pat, range, block) ->
+            let loop = $"(let {pat |> transpilePattern}, {range |> transpileExpr})"
+
+            [ "for"; loop; block |> transpileBlock ] |> String.concat " "
+        | Return expr -> expr |> transpileExpr |> (fun s -> "return " + s)
+
+    and transpileBlock (block: Block) =
+        let statements =
+            block.Statements
+            |> Seq.map transpileStatement
+            |> Seq.map (fun s ->
+                s
+                |> FSharpPlus.String.split [ "\n" ]
+                |> Seq.map (fun s -> "    " + s + "\n")
+                |> String.concat "")
+            |> String.concat ""
+
+        let return_ =
+            block.Return
+            |> Option.map (fun expr -> expr |> transpileExpr |> (fun s -> "    " + s + "\n"))
+            |> Option.defaultValue ""
+
+        "{\n" + statements + return_ + "}"
+
+    let transpile (program: Statement list) =
+        program |> Seq.map transpileStatement |> String.concat "\n"
+
+//
+
+open FParsec
+
 let path = "sample/sample.phis"
 
 let source = System.IO.File.ReadAllText path
-
-// let source = "var x = 0; var y = 1; x + y"
 
 printfn "%s" source
 
@@ -493,4 +646,11 @@ let parser = Parser.program
 
 let ast = source |> FParsec.CharParsers.run parser
 
-printfn "%A" ast
+match ast with
+| Failure(msg, _, _) -> printfn "FAILED!\n%s" msg
+| Success(ast, _, _) ->
+    printfn "ast:\n%A" ast
+
+    let transpiled = Transpiler.transpile ast
+
+    printfn "output:\n%s" transpiled
