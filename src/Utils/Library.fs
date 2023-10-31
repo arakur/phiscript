d0ea5c107a978702022604eafbd332b16d6c752c
arakur <arakur65536@gmail.com>
2023-10-31 15:11:09 +0900

    Created Utils Library.

---
+++ src/Utils/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace Utils
+
+open FSharpPlus
+
+module private Result =
+    let sequence (result: Result<'T, 'Error> seq) =
+        result
+        |> Seq.fold
+            (fun acc result ->
+                monad {
+                    let! acc = acc
+                    let! result = result
+                    return result :: acc
+                })
+            (Ok [])
+        |> Result.map Seq.rev
+
+    let assertWith (condition: bool) (error: 'Error) = if condition then Ok() else Error error
